67f00843d1fee55a4a501c6eaec90f9d81fa485a
takashika11 <cm.takashis@gmail.com>
2025-07-12 23:55:03 +0900

    Layout.astroの作成と、トップページのレイアウト化

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,3 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
 export const SITE_TITLE = "Takashi Shikan's Portfolio";
 export const SITE_DESCRIPTION = '鹿肝　崇のポートフォリオサイトです。';
+export const JOB_TITLE = 'Web Developer & UX Designer';
